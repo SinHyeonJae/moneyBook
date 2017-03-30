5e53529048fed5f706d9bc0560c8dd9d494e143f
SinHyeonJae <sin9033@gmail.com>
2017-03-30 16:54:00 +0900

    Add files via upload

---
+++ Member.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Member {
+	public static String id_index;
+	public static String id;
+	public static String nick;
+	public static String pwd;
+}
---
+++ MoneyBook.java
@@ -0,0 +1,10 @@
+package model;
+
+public class MoneyBook {
+	public static String moneyBookNo;
+	public static String id_index;
+	public static String category;
+	public static String detail;
+	public static String price;
+	public static String date;
+}
