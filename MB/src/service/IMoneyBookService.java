079c40b4b1d0318e46348582633d0dd28c878d9c
kangmingii <gh1541@gmail.com>
2017-04-03 15:43:26 +0900

    Add files via upload

---
+++ MB/src/service/IMoneyBookService.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+import java.util.List;
+
+import model.MoneyBook;
+
+public interface IMoneyBookService {
+	public List<MoneyBook> getMonthContent(MoneyBook mb);
+	public int totalMonthAmount(int price, int id_index);
+	public int totalAmountByCategory(int price, String category, int id_index);
+	public List<MoneyBook> getDayContent(Date date, int id_index);
+	public int totalDayAmount(int price, int id_index);
+	public Date searchDate(Date date);
+	public int moneyBookRegist(MoneyBook mb);
+	public int moneyBookSelectOne(int moneyBookNo, int id_index, Date date);
+	public int moneyBookDelete(int moneyBookNo, int id_index);
+	public int moneyBookUpdate(String category, String detail,
+			int price, int moneyBookNo, int id_index);
+
+}
---
+++ MB/src/service/MoneyBookService.java
@@ -0,0 +1,4 @@
+
+public class MoneyBookService implements IMoneyBookService {
+	
+}
