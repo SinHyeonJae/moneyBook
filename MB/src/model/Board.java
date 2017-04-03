d5b1eb6a914dd67cb3c6d067b51d155085358b0e
Kinuk97 <kiukangg@gmail.com>
2017-04-03 17:05:59 +0900

    Update Board.java

--- MB/src/model/Board.java
+++ MB/src/model/Board.java
@@ -1,14 +1,101 @@
 package model;
 
 public class Board {
-	public static String boardNo;
-	public static String title;
-	public static String content;
-	public static String annotation;
-	public static String date;
-	public static String ageType;
-	public static String nick;
-	public static String id_index;
-	public static String viewNo;
-	public static String recommend;
+	private String boardNo;
+	private String title;
+	private String content;
+	private String annotation;
+	private String date;
+	private String ageType;
+	private String nick;
+	private String id_index;
+	private String viewNo;
+	private String recommend;
+
+	public String getBoardNo() {
+		return boardNo;
+	}
+
+	public void setBoardNo(String boardNo) {
+		this.boardNo = boardNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getAnnotation() {
+		return annotation;
+	}
+
+	public void setAnnotation(String annotation) {
+		this.annotation = annotation;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getAgeType() {
+		return ageType;
+	}
+
+	public void setAgeType(String ageType) {
+		this.ageType = ageType;
+	}
+
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	public String getId_index() {
+		return id_index;
+	}
+
+	public void setId_index(String id_index) {
+		this.id_index = id_index;
+	}
+
+	public String getViewNo() {
+		return viewNo;
+	}
+
+	public void setViewNo(String viewNo) {
+		this.viewNo = viewNo;
+	}
+
+	public String getRecommend() {
+		return recommend;
+	}
+
+	public void setRecommend(String recommend) {
+		this.recommend = recommend;
+	}
+
+	@Override
+	public String toString() {
+		return "Board [boardNo=" + boardNo + ", title=" + title + ", content=" + content + ", annotation=" + annotation
+				+ ", date=" + date + ", ageType=" + ageType + ", nick=" + nick + ", id_index=" + id_index + ", viewNo="
+				+ viewNo + ", recommend=" + recommend + "]";
+	}
 }
