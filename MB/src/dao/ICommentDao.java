32b89b2e3bf10d1213c5cf69bdeedc2cc9e096de
Kinuk97 <kiukangg@gmail.com>
2017-04-05 15:34:04 +0900

    Update ICommentDao.java
    메소드를 추가하고 아마 기존의 메소드도 수정했을거에요

--- MB/src/dao/ICommentDao.java
+++ MB/src/dao/ICommentDao.java
@@ -1,12 +1,21 @@
 package dao;
 
 import java.util.HashMap;
+import java.util.List;
 
 public interface ICommentDao {
-	public int insertComment(HashMap<String, Object> params);
-	public int updateComment(HashMap<String, Object> params);
-	public int deleteComment(HashMap<String, Object> params);
-	public HashMap<String, Object> selectWriterIndex(String nick);
-	public HashMap<String, Object> selectCommentLoc(int commentNo);		
+	public boolean insertComment(HashMap<String, Object> params);
 
+	public boolean updateComment(HashMap<String, Object> params);
+
+	public boolean deleteComment(int commentNo);
+
+	public int selectWriterIndex(String nick);
+
+	public int selectCommentLoc(int commentNo);
+
+	public List<HashMap<String, Object>> selectBoardComment(int boardNo);
+
+	// 코멘트 번호랑 게시글 번호
+	public HashMap<String, Object> selectOneComment(HashMap<String, Object> params);
 }
