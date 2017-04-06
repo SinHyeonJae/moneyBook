d3878a7851dc61ba673dd74df1cefee2e46631cb
Kinuk97 <kiukangg@gmail.com>
2017-04-06 15:34:45 +0900

    Update IMemberDao.java

--- MB/src/dao/IMemberDao.java
+++ MB/src/dao/IMemberDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import model.Member;
 
 public interface IMemberDao {
-   	public int insertMember(Member m);
+   	public int insertMember(Member member);
    	public int updateMember(Member member);			
    	public int deleteMember(int id_index);		
    	public List<Member> selectAllMember();			
