242de41b3387f3cb199d5b217e073a356b86bdd5
SinHyeonJae <sin9033@gmail.com>
2017-04-06 15:32:09 +0900

    잘만들자 인욱레기야
    ㅇ

---
+++ MB/src/dao/IMemberDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.util.List;
+
+import model.Member;
+
+public interface IMemberDao {
+   	public int insertMember(Member m);
+   	public int updateMember(Member member);			
+   	public int deleteMember(int id_index);		
+   	public List<Member> selectAllMember();			
+   	public Member selectOneMember(int id_index);
+   	public int selectIdIndex(String id);
+   	public int selectNick(String nick);
+
+}
